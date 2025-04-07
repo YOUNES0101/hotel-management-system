8447575ad9e12f645d11d5ed46e5ac00acc513c7
SerSamD <Anas.Haida@emsi-edu.ma>
2025-04-07 13:13:41 +0100

    add static

--- hotel_project/config/urls.py
+++ hotel_project/config/urls.py
@@ -5,5 +5,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('hotel_app.urls')),
-    path('account/',include('accounts.urls'))
+    path('accounts/',include('accounts.urls'))
 ]
